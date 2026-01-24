089e6ea0313c0566c353b4cb9fe802620e7c1f95
14elias <ellamebrahtom1995@gmail.com>
2026-01-24 16:43:12 +0300

    some updates

--- streamlit.py
+++ streamlit.py
@@ -3,21 +3,42 @@ import tempfile
 
 from helper import document_loader
 
+
 st.title("Multi Rag App")
 
-uploaded_file = st.file_uploader("upload file")
-query = st.text_input("Ask a question")
+source_mode = st.selectbox(
+    "Choose input type",
+    ["Upload File", "URL"]
+)
 
-if uploaded_file is not None:
-    st.write('File Name', uploaded_file.name)
-    file_type = uploaded_file.type
+def run_streamlit():
+    # query = st.text_input("Ask a question")
+    if source_mode =='Upload File':
+        uploaded_file = st.file_uploader("upload file")
 
-    with tempfile.NamedTemporaryFile(delete=False, suffix=uploaded_file.name) as tmp:
-        tmp.write(uploaded_file.read())
-        file_path = tmp.name
+        if uploaded_file is not None:
+            st.write('File Name', uploaded_file.name)
+            file_type = uploaded_file.type
 
-    documents = document_loader.load(source=file_path, source_type=file_type)
-    
-    for i, doc in enumerate(documents):
-        st.markdown(f"### Page {i + 1}")
-        st.text(doc.page_content[:3000])
+            with tempfile.NamedTemporaryFile(delete=False, suffix=uploaded_file.name) as tmp:
+                tmp.write(uploaded_file.read())
+                file_path = tmp.name
+
+            documents = document_loader.load(source=file_path, source_type=file_type)
+            
+            for i, doc in enumerate(documents):
+                st.markdown(f"### Page {i + 1}")
+                st.text(doc.page_content[:3000])
+
+    elif source_mode == 'URL':
+        url = st.text_input("Enter webpage URL")
+        if url:
+            documents =document_loader.load(source=url, source_type="web")
+
+            for i, doc in enumerate(documents):
+                st.markdown(f"### Page {i + 1}")
+                st.text(doc.page_content[:3000])
+
+
+
+run_streamlit()
